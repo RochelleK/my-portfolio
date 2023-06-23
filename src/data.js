fb4fc3e2e219b08a83047eb56d83d3d7889c32d7
Rochelle Katzman <zelrochelle@gmail.com>
2023-06-23 16:23:40 -0400

    descriptions

--- src/data.js
+++ src/data.js
@@ -3,31 +3,31 @@ export const projects = [
     title: "Future Funds",
     subtitle: "MERN Stack",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "An app that simplifies retirement planning by accurately calculating your savings trajectory, investment growth, and estimated retirement income, so you can confidently prepare for a secure and worry-free retirement.",
     image: "./project-1.gif",
-    link: "https://google.com",
+    link: "https://github.com/RochelleK/FutureFunds",
   },
   {
     title: "Guidance",
     subtitle: "React and Ruby",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "The ultimate app that revolutionizes your education decisions by calculating the true worth of tuition based on your expected salary, empowering you to make informed choices for a brighter financial future.",
     image: "./guidance-gif.gif",
-    link: "https://google.com",
+    link: "https://github.com/hassanrazak-flatiron/phase-4-project-backend-guidance",
   },
   {
-    title: "DevChat",
+    title: "CodeGames",
     subtitle: "React and Firebase",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "An app that teaches coding syntax through a quiz game format, using the React framework. Level up your coding skills while having fun with CodeGames.",
     image: "./project-3.gif",
-    link: "https://google.com",
+    link: "https://github.com/Aotaola/CodeGames2",
   },
   {
     title: "My Portfolio",
     subtitle: "This site right here :)",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "My portfolio site built with React. Explore a captivating collection of stunning web development projects that showcase my expertise in front-end design, responsive layouts, and interactivity. ",
     image: "./project-4.gif",
     link: "https://github.com/RochelleK/my-portfolio",
   },
@@ -50,11 +50,4 @@ export const testimonials = [
   },
 ];
 
-export const skills = [
-  "JavaScript",
-  "React",
-  "Next.js",
-  "Ruby",
-  "HTML",
-  "CSS",
-];
+export const skills = ["JavaScript", "React", "Next.js", "Ruby", "HTML", "CSS"];
